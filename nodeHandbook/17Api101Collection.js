9126faaebdbb5e00da26b9538a77b2116779633c
Martin Coronel <martincxx@gmail.com>
2020-11-12 22:39:27 +0300

    get a customer

--- nodeHandbook/17Api101Collection.js
+++ nodeHandbook/17Api101Collection.js
@@ -18,4 +18,22 @@ const countCustomers = async () => {
   }
 };
 
+const getACustomer = async (id) => {
+  return await axios.get("https://api-101.glitch.me/customer", {
+    params: { id },
+  });
+};
+
+const printCustomerInfo = async (id) => {
+  const customer = await getACustomer(id);
+  if (customer.data.data.customer) {
+    const { name, type } = customer.data.data.customer;
+
+    console.log({ name });
+    console.log({ type });
+  } else {
+    console.log("No such customer");
+  }
+};
+printCustomerInfo(1);
 countCustomers();
